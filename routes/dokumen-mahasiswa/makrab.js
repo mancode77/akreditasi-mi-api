7eddaab30878834eaa128d7597e9217f4be40ce4
mancode <mancode700@gmail.com>
2023-09-12 20:41:31 +0700

    UPDATE: route makrb delete

--- routes/dokumen-mahasiswa/makrab.js
+++ routes/dokumen-mahasiswa/makrab.js
@@ -10,6 +10,6 @@ const router = express.Router()
 
 router.get('/makrab/:year', getMakrab)
 router.post('/makrab/:year', upload.single('image'), postMakrab)
-router.delete('/makrab/:idMakrab', deleteMakrab)
+router.delete('/makrab/:year/:idMakrab', deleteMakrab)
 
 export default router
